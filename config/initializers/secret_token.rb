21a7852a560bef02b60c190a7af855bd47af8462
jayson camba <jayson.camba@p3ople4u.com>
2012-09-06 16:51:03 +0800

    success on creating an authentication using devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedinApplication::Application.config.secret_token = '48a0d359a8ab950e5e7b2d45a7279c0ab523bd0d69f85a65a47748a1e515011538b331ab373e226d94a7d3e4b90739138f5d2643ad3cf227e29dce646f47c8bc'
